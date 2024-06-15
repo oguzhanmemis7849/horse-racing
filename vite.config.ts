70db42ce5d63bb721432036cb09d46f6810b8ad0
oguzhanmemis7849 <oguzhanmemis266@gmail.com>
2024-06-15 18:43:29 +0300

    build: add some ts and vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
@@ -9,6 +11,9 @@ export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    Components({
+      resolvers: [PrimeVueResolver()]
+    })
   ],
   resolve: {
     alias: {
